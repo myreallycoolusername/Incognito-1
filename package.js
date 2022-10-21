214da1264c51e0bd20c39e17929b96cd869bf27a
myreallycoolusername <91684986+myreallycoolusername@users.noreply.github.com>
2022-10-21 21:41:09 +0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,37 @@
+{
+
+    "name": "Incognito",
+
+    "version": "1.0.0",
+
+    "description": "",
+
+  "scripts": {
+
+        "build": "echo \"No build specified\" && exit 1",
+
+       "test": "echo \"No test specified\" && exit 1",
+
+        "start": "node index.js"
+
+    },
+
+    "repository": {
+
+      "type": "git",
+
+      "url": "git+https://github.com/myreallycoolusername/Incognito.git"
+
+    },
+
+   "author": "",
+
+   "license": "ISC",
+
+   "bugs": {
+
+   "url": "https://github.com/myreallycoolusername/Incognito/issues"
+
+    },
+
+     "homepage": "https://github.com/myreallycoolusername/Incognito#readme
